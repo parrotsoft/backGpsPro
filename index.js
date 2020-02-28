77e7585d2077f61b9b7703f4f96dfe39665f687b
Miguel Lopez <lopezarizamiguel@gmail.com>
2020-02-28 13:57:22 -0500

    feat: implementando cors

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
+app.use(cors())
 
 app.post('/', function(req, res) {
     const date = new Date();
     console.log(`${date.getHours()}:${date.getMinutes()} :: latitude = ${req.body.latitude} - longitude = ${req.body.longitude}`);
-    res.sendStatus(200);
+    res.json({msg: 'Location in...'});
 });
 
 app.listen(3000, function() {
